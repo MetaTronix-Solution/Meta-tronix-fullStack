747bcd128d8b38e46a5530fe162062097de431e6
Ajay4145 <ajaytamang4145@gmail.com>
2026-08-05 18:05:55 +0545

    feat: add createdBy field to blog model and update blog controller methods for ownership validation

--- server/src/controllers/blog.controller.ts
+++ server/src/controllers/blog.controller.ts
@@ -68,6 +68,7 @@ class BlogController {
         author,
         category,
         published,
+        createdBy: req.user!.id,
       });
 
       return res.status(201).json({
@@ -92,7 +93,10 @@ class BlogController {
       throw new AppError("Invalid blog ID.", 400);
     }
 
-    const existingBlog = await Blog.findById(id);
+    const existingBlog = await Blog.findOne({
+      _id: id,
+      createdBy: req.user!.id,
+    });
 
     if (!existingBlog) {
       if (req.file) {
@@ -154,10 +158,17 @@ class BlogController {
     }
 
     try {
-      const updatedBlog = await Blog.findByIdAndUpdate(id, updateData, {
-        new: true,
-        runValidators: true,
-      });
+      const updatedBlog = await Blog.findOneAndUpdate(
+        {
+          _id: id,
+          createdBy: req.user!.id,
+        },
+        updateData,
+        {
+          new: true,
+          runValidators: true,
+        },
+      );
 
       if (req.file && existingBlog.imageUrl) {
         await deleteUpload(existingBlog.imageUrl);
@@ -184,7 +195,10 @@ class BlogController {
       throw new AppError("Invalid blog ID.", 400);
     }
 
-    const blog = await Blog.findById(id);
+    const blog = await Blog.findOne({
+      _id: id,
+      createdBy: req.user!.id,
+    });
 
     if (!blog) {
       throw new AppError("Blog not found.", 404);
